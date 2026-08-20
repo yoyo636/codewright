ed2a247c8c8bc4cab059c1572e678993984f0df6
yoyo636 <13520123772@163.com>
2026-08-20 21:42:38 +0800

    chore: bump homebrew formula to 3.0.2-beta
    Update version and sha256 digests for darwin x64/arm64 and
    linux x64/arm64 from the v3.0.2-beta release assets.

--- script/homebrew/codewright.rb
+++ script/homebrew/codewright.rb
@@ -17,7 +17,7 @@ class Codewright < Formula
   desc "AI coding agent for the terminal"
   homepage "https://github.com/yoyo636/codewright"
   license "MIT"
-  version "3.0.1-beta"
+  version "3.0.2-beta"
 
   # SHA256 digests are taken from the GitHub release assets for this version.
   # To update them for a new release, run:
@@ -26,24 +26,24 @@ class Codewright < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/yoyo636/codewright/releases/download/v#{version}/codewright-darwin-x64.zip"
-      sha256 "b8faf9adcc36bb731defe1859f8958b30fceed733779d60b0f520c6e34ae642d"
+      sha256 "4e52a68f41372f67c90fccfdc344b30c3f360ad8c242ba85c3f6d5a56f1fd246"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/yoyo636/codewright/releases/download/v#{version}/codewright-darwin-arm64.zip"
-      sha256 "b966b04b86a98321b106ac09936471e46f14a883f44a6c0696a7dfe384d37d9b"
+      sha256 "9b32de0b224082096d67ee29b8a18e67a3ddcd43b2d15e7c146b6690242bd804"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/yoyo636/codewright/releases/download/v#{version}/codewright-linux-x64.tar.gz"
-      sha256 "55d880c8dc10223ff717bf3e76a89744b0889c0e9c2e34f7280cbeced1862cb6"
+      sha256 "37bc634c9ba54dad7722184bd69719b0a52aba91e180385a40ebde929a67d245"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/yoyo636/codewright/releases/download/v#{version}/codewright-linux-arm64.tar.gz"
-      sha256 "c216dd896e47eafbf476010b0bcda6de52b49d901822443448938bab0d133edf"
+      sha256 "4793e7a40174fcc6fdedadaf1f20c6d2c9a56503d541d4be1de332b145b259ed"
     end
   end
 
